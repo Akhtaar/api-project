353a7f52c53170dd636787f971caba2da983c99e
Akhtaar <khalid88.ayachia@gmail.com>
2019-11-14 11:43:03 +0100

    Change index

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express"), // Express - System route pour REST API
+    mysql = require("mysql"), // Package de connection BDD MySqli
+    bodyParser = require("body-parser"), // Middleware pour la data das le body
+    bcrypt = require("bcrypt"), // Une bibliothèque pour vous aider à hacher les mots de passe
+    app = express(), // Création d'un application REST
+    port = 3000, // Port de connection
+    connect = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "api_project"
+    });
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port port!`))
